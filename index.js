044a39d6527e5c7b519831fcd9d367282d29c05b
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-01-25 20:25:58 -0500

    added manager, engineer and inter sections

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const templateHTML = require('./src/templateHTML.js')
+
 // require all of your classes
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
@@ -8,16 +10,13 @@ const Employee = require('./lib/Employee');
 // require inquirer, path if needed, fs
 const inquirer = require('inquirer');
 const fs = require('fs');
-const createTeam = require('./src/page-template.js')
+
 
 // empty team members array
-const newTeamMember = [];
-
-// empty team members array
+const teamArray = [];
 
 // empty id array
-const questions = async () => {
-  const answers = await inquirer.prompt( [
+const employeeInput = [
     
     {
       type: 'input',
@@ -28,14 +27,14 @@ const questions = async () => {
 
     {
       type: 'input',
-      message: 'Enter ID: ',
+      message: 'Enter ID number: ',
       name: 'id',
 
     },
 
     {
       type: 'input',
-      message: 'Enter email: ',
+      message: 'Enter email address: ',
       name: 'email',
 
     },
@@ -49,40 +48,113 @@ const questions = async () => {
     },
 
   
-  ]
+  ];
 
-  )
-}
-//Answers
-// function that wraps everything (like an init function)
+  // function that wraps everything (like an init function)
 
-  // function for creating a manager (call this at the bottom of your init function)
+  const promptEmployee = () => {
+    return inquirer.prompt(employeeInput)
+  }
+
+
+    // function for creating a manager (call this at the bottom of your init function)
     // inquirer prompt with the manager questions
     // in your .then - set up a variable for manager that is equal to a new instance of your Manager class passing in the responses you recieved from the user's input
-      .then((answers) => {
-        const manager = new Manager (answers.name)
-        teamMember.push(manager);
-      })
-    // push that variable to your team members array, push the id to the id array
-      // call your next function
+
+    const managerInput = () => { [
+      
+      {
+        type: 'input',
+        message: 'Enter office number: ',
+        name: 'office',
+      }
+      ]
+
+        return inquirer.prompt(managerInput)
+        
+       .then((answers) => {
+        const manager = new Manager (
+          answers.name, 
+          answers.id, 
+          answers.email, 
+          answers.office
+          );
+           // push that variable to your team members array, push the id to the id array
+        employeeInput.push(manager);
+       
+       
+      })}
+
+      const engineerInput = () => { [
+      
+        {
+          type: 'input',
+          message: 'Enter GitHub link: ',
+          name: 'github',
+        }
+        ]
+  
+          return inquirer.prompt(engineerInput)
+          
+         .then((answers) => {
+          const engineer = new Engineer (
+            answers.name, 
+            answers.id, 
+            answers.email, 
+            answers.github
+            );
+             // push that variable to your team members array, push the id to the id array
+          employeeInput.push(engineer);
+       
+         
+        })}
+
+        const internInput = () => { [
+      
+          {
+            type: 'input',
+            message: 'Enter School name: ',
+            name: 'school',
+          }
+          ]
+    
+            return inquirer.prompt(internInput)
+            
+           .then((answers) => {
+            const intern = new Intern (
+              answers.name, 
+              answers.id, 
+              answers.email, 
+              answers.school
+              );
+               // push that variable to your team members array, push the id to the id array
+            employeeInput.push(intern);
+          
+         
+          })}
+    
+     
+   //call your next function
+     
 
   // next function should be for creating the team
     // this should ask the user what type of employee they would like to create
     // in your .then, have a conditional or switch case that runs that employee types function based on what they choose
     // or if they select the option that says they are done adding team members, run the function that builds the team
 
-
+    // fs.writeFileSync(yourPathToDistFolder, functionFromPageTemplate(teamMembers), 'utf-8');
     function buildTeam() {
-      console.log('New team member added:', newTeamMember);
+
+      console.log('New team member added:', teamArray);
       fs.writeFileSync(
         './dist/team.html',
-        createTeam(newTeamMember);
-        
-      )
-        // fs.writeFileSync(yourPathToDistFolder, functionFromPageTemplate(teamMembers), 'utf-8');
+        createTeam(teamArray),
+        'utf-8'
+         );
+      
       }
   
-      questions()
+      promptEmployee()
   // make sure call your init function
   
   
--- src/templateHTML.js
+++ src/templateHTML.js
@@ -1,4 +1,4 @@
-module.exports = myTeam {
+module.exports = templateHTML; {
 
 return ` <!DOCTYPE html>
 <html lang="en">
