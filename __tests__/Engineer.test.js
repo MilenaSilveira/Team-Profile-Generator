2af2e15f7ed32095c2f76d28583049c9f300af21
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-01-26 17:53:53 -0500

    Added engineer and employee test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,32 @@
 const Engineer = require('../lib/Engineer');
+
+test('Generate Engineer', () => {
+    const engineer = new Engineer('Milena', 33, 'milenacsilveira@gmail.com', 'https://github.com/MilenaSilveira/Team-Profile-Generator');
+    
+    expect(engineer.getName()) .toEqual('Milena');
+});
+
+test('Access Engineer Id', () => {
+    const engineer = new Engineer('Milena', 33, 'milenacsilveira@gmail.com', 'https://github.com/MilenaSilveira/Team-Profile-Generator');
+    
+    expect(engineer.getId()) .toEqual('33');
+});
+
+test('Access Engineer Email', () => {
+    const engineer = new Engineer('Milena', 33, 'milenacsilveira@gmail.com', 'https://github.com/MilenaSilveira/Team-Profile-Generator');
+    
+    expect(engineer.getEmail()).toEqual('milenacsilveira@gmail.com');
+});
+
+test('Access Engineer Github', () => {
+    const engineer = new Engineer('Milena', 33, 'milenacsilveira@gmail.com', 'https://github.com/MilenaSilveira/Team-Profile-Generator');
+    
+    expect(engineer.getGithub()).toEqual('https://github.com/MilenaSilveira/Team-Profile-Generator');
+});
+
+
+test('Access Engineer role', () => {
+    const engineer = new Engineer('Milena', 33, 'milenacsilveira@gmail.com', 'https://github.com/MilenaSilveira/Team-Profile-Generator');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+}); 
